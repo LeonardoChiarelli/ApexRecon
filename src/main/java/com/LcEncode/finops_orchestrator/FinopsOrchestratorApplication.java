0b05ce9ffb9933f5a2c7c43d501efa23bfe59535
Leonardo Chiarelli <devleochiarelli@gmail.com>
2025-11-05 23:36:23 -0300

    Initial commit

---
+++ src/main/java/com/LcEncode/finops_orchestrator/FinopsOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.LcEncode.finops_orchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinopsOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinopsOrchestratorApplication.class, args);
+	}
+
+}
